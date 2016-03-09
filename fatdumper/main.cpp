40394829b5995b78441e028f050d12dfaf91056a
Joël Luijmes <joelluijmes@gmail.com>
2016-03-09 17:35:51 +0100

    Moved testing to new project (fat is now static lib)

---
+++ fatdumper/main.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+
+const char* path = "C:\\Users\\joell\\Dropbox\\Documenten\\sd.bin";
+
+using namespace std;
+
+fstream file(path, ios_base::in | ios_base::binary);
+
+uint8_t fetch(unsigned address, unsigned count, char* out)
+{
+    file.seekg(address);
+    file.get(out, count);
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    fat_BootSector boot;
+    unsigned i = sizeof(fat_BootSector);
+    uint32_t offset = fat_nextPartitionSector(fetch, &boot, nullptr);
+    //fetch(offset, sizeof(boot), (char*)&boot);
+    FatType type = fat_getType(&boot);
+
+    uint32_t first = fat_firstDataSector(&boot);
+    uint32_t rootDirSectors = fat_numberOfRootDirSectors(&boot);
+    uint32_t firstSectorOfCluster = fat_firstSectorOfCluster(&boot, 2);
+
+    uint32_t firstRoot = boot.reservedSectors + (boot.numberOfFATs * boot.sectorsPerFAT16);
+
+    fat_DirectoryEntry entry = { 0 };
+    char buf[255];
+
+    while (fat_nextDirectoryEntry(&boot, offset, 2, fetch, &entry, buf, 255))
+    {
+        cout << buf << endl;
+    }
+
+    return 0;
+}
---
+++ fatdumper/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#pragma comment(lib, "fat.lib")
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <cinttypes>
+#include <iostream>
+#include <fstream>
+
+extern "C" {
+#include "fat.h"
+}
+
+// TODO: reference additional headers your program requires here
